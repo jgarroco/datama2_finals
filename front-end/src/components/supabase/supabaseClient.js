c780c8c1ecffe06278fc63f5636e5c0111155d3c
Christian Esguerra <ciesguerra2@student.apc.edu.ph>
2025-02-28 06:32:31 +0000

    Fixed supabaseKey

--- front-end/src/components/supabase/supabaseClient.js
+++ front-end/src/components/supabase/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://hiokebybfeuerkrmmrdv.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhpb2tlYnliZmV1ZXJrcm1tcmR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0NjU1ODksImV4cCI6MjA1NjA0MTU4OX0.t-xG6DDjpFOqlNBUrZQumXtuYJ1ZeIbX1rFFu2ij1x4'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
