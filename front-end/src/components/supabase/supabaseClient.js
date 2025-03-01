aa2f2f70bcfe6fed3fd5a287c0fec20561d480c2
chrstn-sgrr <ciesguerra2@student.apc.edu.ph>
2025-03-01 15:32:20 +0800

    Line change? Lol

--- front-end/src/components/supabase/supabaseClient.js
+++ front-end/src/components/supabase/supabaseClient.js
@@ -4,4 +4,4 @@ const supabaseUrl = 'https://hiokebybfeuerkrmmrdv.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhpb2tlYnliZmV1ZXJrcm1tcmR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0NjU1ODksImV4cCI6MjA1NjA0MTU4OX0.t-xG6DDjpFOqlNBUrZQumXtuYJ1ZeIbX1rFFu2ij1x4'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-export default supabase;
+export default supabase; 
